39b6f7d5baab5e5689f5ebcf2e29a49fdb989452
RaghavPal <raghav.pal@testingxperts.com>
2017-11-14 16:37:39 +0530

    added code to TestBrokenLinks

---
+++ SeleniumTips/src/test/java/seleniumtips/TestAllLinks.java
@@ -0,0 +1,69 @@
+package seleniumtips;
+
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Iterator;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TestAllLinks {
+
+	@Test
+	public void testLinks() {
+
+		WebDriver driver = new ChromeDriver();
+
+		// Step 1. Navigate to the url
+		driver.get("http://opensource.demo.orangehrmlive.com/");
+
+		// Step 2. Find all the links on the webpage and store in a list variable
+		List<WebElement> links = driver.findElements(By.tagName("a"));
+
+		// Step 3 : Start a loop with a counter equal to the number of links
+		Iterator<WebElement> it = links.iterator();
+
+		String url="";
+		HttpURLConnection connection = null;
+		int responseCode=200;
+
+		while(it.hasNext()){
+
+			url = it.next().getAttribute("href");
+
+			System.out.println("Checking link : "+url);
+
+			if(url == null || url.isEmpty()){
+				System.out.println("URL is either not configured for anchor tag or it is empty");
+				continue;
+			}
+
+
+			try {
+				connection = (HttpURLConnection)(new URL(url).openConnection());
+
+				connection.setRequestMethod("HEAD");
+
+				connection.connect();
+
+				responseCode = connection.getResponseCode();
+
+				if(responseCode >= 400){
+					System.out.println(url+" is a broken link");
+				}
+				else{
+					System.out.println(url+" is a valid link\n");
+				}
+
+			} catch (Exception e) {
+				e.printStackTrace();
+			} 
+		}
+
+	}
+}
